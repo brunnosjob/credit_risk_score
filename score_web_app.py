e7604b45573a6c63b7acd5cea74be34346015ec2
Bruno Rodrigues <74928057+brunnosjob@users.noreply.github.com>
2023-07-26 20:27:15 -0300

    Update score_web_app.py

--- score_web_app.py
+++ score_web_app.py
@@ -6,4 +6,4 @@ import pickle
 # Importando algoritmo
 from sklearn.ensemble import RandomForestClassifier
 
-st.markdown('*__Observação: para mais informações, navegue pelas páginas__*')
+st.markdown('__Observação: para mais informações, navegue pelas páginas__')
