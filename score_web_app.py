d71065aa0aefe22ddb09df7f07f8c201c5e35ea7
Bruno Rodrigues <74928057+brunnosjob@users.noreply.github.com>
2023-07-28 22:40:29 -0300

    Update score_web_app.py

--- score_web_app.py
+++ score_web_app.py
@@ -26,9 +26,80 @@ mensalidade = (round(((financiamento/parcela)) * juro,2))
 
 if parcela == 1:
     st.write('Valor à vista se o empréstimo for aprovado: R$ {} por mês'.format(mensalidade))
+
+     modelo_RFC = joblib.load('modelo_RFC_v2.joblib')
+    
+    def classificacao_risco(renda, percentual_comprometimento):
+        
+        if renda <= 2000 and percentual_comprometimento <= 0.3:
+            risco = 1
+        elif renda <= 2000 and percentual_comprometimento <= 0.5:
+            risco = 3
+        elif renda <= 2000 and percentual_comprometimento <= 1.0:
+            risco = 7
+        elif renda <= 5000 and percentual_comprometimento <= 0.3:
+            risco = 1
+        elif renda <= 5000 and percentual_comprometimento <= 0.5:
+            risco = 2
+        elif renda <= 5000 and percentual_comprometimento <= 0.1:
+            risco = 6
+        elif renda <= 10000 and percentual_comprometimento <= 0.3:
+            risco = 1
+        elif renda <= 10000 and percentual_comprometimento <= 0.5:
+            risco = 1
+        elif renda <= 10000 and percentual_comprometimento <= 1.0:
+            risco = 5
+        elif renda > 10000 and percentual_comprometimento <= 0.3:
+            risco = 1
+        elif renda > 10000 and percentual_comprometimento <= 0.5:
+            risco = 1
+        elif renda > 10000 and percentual_comprometimento <= 1.0:
+            risco = 4
+    
+        return risco
+    
+    def conversao_opcao_moradia(opcao):
+      if opcao == 'Própria':
+        output = 1
+      elif opcao == 'Hipoteca':
+        output = 2
+      elif opcao == 'Aluguel':
+        output = 3
+      elif opcao == 'Outro':
+        output = 4
+      return output
+    
+    def conversao_objetivo_financiamento(opcao):
+      if opcao == 'Empreendimento':
+        output = 1
+      elif opcao == 'Educação':
+        output = 2
+      elif opcao == 'Vestuário/Entretenimento':
+        output = 3
+      elif opcao == 'Saúde':
+        output = 4
+      elif opcao == 'Reforma':
+        output = 5
+      elif opcao == 'Quita outra dívida':
+        output = 6
+      return output
+    
+    # Backend
+    condicao_moradia = conversao_opcao_moradia(moradia)
+    objetivo = conversao_objetivo_financiamento(objetivo_financiamento)
+    percentual_comprometimento = (financiamento/renda)
+    juro = juro
+    risco_credito = classificacao_risco(renda, percentual_comprometimento)
+    
+    X_final = [[percentual_comprometimento, renda, risco_credito, condicao_moradia, juro, financiamento, objetivo]]
+    
+    score_final = modelo_RFC.predict_proba(X_final) * 1000
+    
+    st.markdown(score_final[0][0])
+
+
 else:
     st.write('Se o empréstimo for aprovado, a mensalidade fica de R$ {} por mês'.format(mensalidade))
-
 
     modelo_RFC = joblib.load('modelo_RFC_v2.joblib')
     
@@ -98,5 +169,5 @@ else:
     
     score_final = modelo_RFC.predict_proba(X_final) * 1000
     
-    st.markdown('*_Observação: para mais informações, navegue pelas páginas_*')
+    st.markdown(score_final[0][0])
     
