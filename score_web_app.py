b06d7bf073b6d58161614ea305ce22a1a57fc157
Bruno Rodrigues <74928057+brunnosjob@users.noreply.github.com>
2023-07-28 23:04:28 -0300

    Update score_web_app.py

--- score_web_app.py
+++ score_web_app.py
@@ -103,7 +103,7 @@ score_final = modelo_RFC.predict_proba(X_final) * 1000
 if parcela == 1:
     st.write('Valor à vista se o empréstimo for aprovado: R$ {} por mês'.format(mensalidade))
     st.markdown('Score:')
-    st.markdown("**<h1>{score_final[0][0]}</h1>**")
+    st.markdown(f"**{score_final[0][0]}**")
     
 # Condicional para pagamento em mais de 1 vez
 else:
